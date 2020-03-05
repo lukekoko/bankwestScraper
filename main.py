4d552a65b5577f5bb7c90c535b086912024d1541
luke <lukekoko1@gmail.com>
2020-03-05 21:23:44 +1100

    scraper done

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ import time
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
 import logging
 import logging.config
+import pathlib
 from config import config
+import pandas
 
 settings = config.settings
 logging.config.fileConfig(
@@ -12,22 +15,36 @@ logging.config.fileConfig(
 )
 logger = logging.getLogger(__name__)
 
-chrome_options = Options()  
-# chrome_options.add_argument("--headless")
-chromeDriver = "./config/chromedriver"
+def browserConfig():
+    logger.info("Configuring browser")
+    chrome_options = Options()  
+    chrome_options.add_argument("--headless")
+    prefs = {
+        'download.default_directory' : str(pathlib.Path('./downloads/').resolve()),
+        "directory_upgrade": True
+    }
+    chrome_options.add_experimental_option('prefs', prefs)
+    chromeDriver = "./config/chromedriver"
 
-login_page = "https://ibs.bankwest.com.au/BWLogin/rib.aspx"
+    browser = webdriver.Chrome(executable_path=chromeDriver, options=chrome_options)
+    return browser
 
 def scraper():
-    browser = webdriver.Chrome(executable_path=chromeDriver, options=chrome_options)
-    browser.set_window_size(1080,1920)
-    browser.get(login_page)
+    logger.info("Starting")
+    browser = browserConfig()
+    browser.get(settings['url'])
 
+    logger.info("Logging in")
     browser.find_element_by_id("AuthUC_txtUserID").send_keys(settings['login'])
     browser.find_element_by_id("AuthUC_txtData").send_keys(settings['password'])
     browser.find_element_by_id("AuthUC_btnLogin").click()
-    
-    time.sleep(20)
+
+    browser.find_element_by_link_text(settings['account']).click()
+    browser.find_element_by_tag_name('html').send_keys(Keys.END)
+
+    logger.info("Downloading")
+    browser.find_element_by_id("_ctl0_ContentButtonsRight_btnExport").click()
+    time.sleep(5)
 
 if __name__ == "__main__":
     scraper()
